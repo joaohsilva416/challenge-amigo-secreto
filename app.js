b74a80a932cddef5f588f1bd3816142bf3326022
joaohsilva <joaohasilva416@gmail.com>
2025-01-17 12:20:59 -0300

    Função atualizarAmigos completa

--- app.js
+++ app.js
@@ -5,12 +5,26 @@ function adicionarAmigo() {
     console.log(nome);
 
     if (nome.trim() === "") {
-        alert("Por favor, insira um nome!")
+        alert("Por favor, insira um nome!");
     } else if(!amigos.includes(nome)) {
         amigos.push(nome.trim());
         document.getElementById("amigo").value = "";
         console.log(amigos);
+        atualizarAmigos();
     } else {
         alert("Este nome já foi adicionado!");
     }
+}
+
+function atualizarAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    amigos.forEach(nome => {
+        let newEl = document.createElement('li');
+        newEl.textContent = nome;
+        console.log(newEl);
+        lista.appendChild(newEl);
+        console.log(lista);
+    });
 }
