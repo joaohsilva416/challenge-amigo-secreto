f1295ac30672f87c6b237000dfe7d1fb7040ffb3
joaohsilva <joaohasilva416@gmail.com>
2025-01-17 10:58:10 -0300

    Melhorando a função adicionarAmigos

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ function adicionarAmigo() {
     let nome = document.getElementById("amigo").value;
     console.log(nome);
 
-    if (nome == "") {
+    if (nome.trim() === "") {
         alert("Por favor, insira um nome!")
-    }
-    
-    if (nome != "") {
-        amigos.push(nome);
+    } else if(!amigos.includes(nome)) {
+        amigos.push(nome.trim());
         document.getElementById("amigo").value = "";
-        console.log(amigos)
+        console.log(amigos);
+    } else {
+        alert("Este nome já foi adicionado!");
     }
 }
